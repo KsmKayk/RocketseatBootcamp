da65dfe7803abe25f340c86fa2a38dd5a1840a69
Kayk Mascarenhas <ksm.kayk@outlook.com>
2020-11-18 14:18:18 -0300

    Acabado modulo 2

--- modulo02/backend/src/index.js
+++ modulo02/backend/src/index.js
@@ -1,24 +1,61 @@
-const { query } = require('express')
 const express = require('express')
+const {uuid, isUuid} = require('uuidv4')
 
 const app = express()
 app.use(express.json())
 
+const projects = []
+
+function logRequest(req, res, next) {
+  const {method, url} = req
+  const logLabel = `[${method}] ${url}`
+  console.time(logLabel)
+  next()
+  console.timeEnd(logLabel)
+}
+
+function validadeProjectId(req, res, next) {
+  const {id} = req.params
+  if (!isUuid(id)) {
+    return res.status(400).json({error:"Invalid project ID"})
+  }
+  return next()
+}
+
+app.use(logRequest)
+app.use("/projects/:id",validadeProjectId)
+
 app.get('/projects', (req, res) => {
-  const query = req.query
-  return res.json(["Projeto 1", "Projeto 2", "Projeto 3", [query]])
+  const {title} = req.query
+  const results = title ? projects.filter(project => project.title.includes(title)) : projects
+  return res.json(results)
 })
 app.post('/projects', (req, res) => {
-  const body = req.body
-  return res.json(["Projeto 1", "Projeto 2", "Projeto 3", "Projeto 4",[body]])
+  const {title, owner} = req.body
+  const project = {id: uuid(), title, owner}
+  projects.push(project)
+  return res.json(project)
 })
 app.put('/projects/:id', (req, res) => {
-  const params = req.params
-  return res.json(["Projeto 1 Editado", "Projeto 2 Editado", "Projeto 3 Editado", "Projeto 4 Editado", [params]])
+  const {id} = req.params
+  const {title, owner} = req.body
+  const projectIndex = projects.findIndex(project => project.id === id)
+  
+  if(projectIndex < 0) {
+    return res.status(400).json({ error: 'Project not found' })
+  }
+  const project = {id,title,owner}
+  projects[projectIndex] = project
+  return res.json(project)
 })
 app.delete('/projects/:id', (req, res) => {
-  const params = req.params
-  return res.json(["Projeto 1 Editado", "Projeto 2 Editado", "Projeto 3 Editado", [params]])
+  const {id} = req.params
+  const projectIndex = projects.findIndex(project => project.id === id)
+  if(projectIndex < 0) {
+    return res.status(400).json({ error: 'Project not found' })
+  }
+  projects.splice(projectIndex, 1)
+  return res.status(204).send()
 })
 
 app.listen(3333, () => {
