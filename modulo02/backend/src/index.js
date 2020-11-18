d73ebcc8971e3db131195114ba88b11affd9a8df
Kayk Mascarenhas <ksm.kayk@outlook.com>
2020-11-18 13:04:21 -0300

    Estudo dos parametros e metodos http

--- modulo02/backend/src/index.js
+++ modulo02/backend/src/index.js
@@ -1,9 +1,26 @@
+const { query } = require('express')
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
-app.get('/mensagem', (req, res) => {
-  return res.json({message: 'Hello World'})
+app.get('/projects', (req, res) => {
+  const query = req.query
+  return res.json(["Projeto 1", "Projeto 2", "Projeto 3", [query]])
+})
+app.post('/projects', (req, res) => {
+  const body = req.body
+  return res.json(["Projeto 1", "Projeto 2", "Projeto 3", "Projeto 4",[body]])
+})
+app.put('/projects/:id', (req, res) => {
+  const params = req.params
+  return res.json(["Projeto 1 Editado", "Projeto 2 Editado", "Projeto 3 Editado", "Projeto 4 Editado", [params]])
+})
+app.delete('/projects/:id', (req, res) => {
+  const params = req.params
+  return res.json(["Projeto 1 Editado", "Projeto 2 Editado", "Projeto 3 Editado", [params]])
 })
 
-app.listen(3333)
+app.listen(3333, () => {
+  console.log("back-end has started! 💜")
+})
