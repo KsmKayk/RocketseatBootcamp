0234dfeabac4157e2ce09b55eb1156da231eab45
Kayk Mascarenhas <ksm.kayk@outlook.com>
2021-02-08 12:35:25 -0300

    iniciado backend com node.js e typescript, configurado typescript e ts-node-dev

---
+++ modulo06-07-08-09/backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.json({message: "Hello World! 💜"})
+})
+
+app.listen(3333, () => {
+  console.log('💜 Server started on port: 3333')
+})
