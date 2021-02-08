d3fbab70687309bfcb805b5af0e5fda7ca478ff5
Kayk Mascarenhas <ksm.kayk@outlook.com>
2021-02-08 19:10:44 -0300

    criado appointmentsRepository e organizado mais o codigo

--- modulo06-07-08-09/backend/src/routes/appointments.routes.ts
+++ modulo06-07-08-09/backend/src/routes/appointments.routes.ts
@@ -1,26 +1,24 @@
 import { Router } from "express"
 import { startOfHour, parseISO, isEqual} from "date-fns"
-import Appointment from "../models/Appointment"
+import AppointmentsRepository from "../repositories/AppointmentsRepository"
 
 const appointmentsRouter = Router()
+const appointmentsRepository = new AppointmentsRepository()
 
-const appointments:Appointment[] = []
 
 appointmentsRouter.post("/", (req, res) => {
   const {provider, date} = req.body
 
   const parsedDate = startOfHour(parseISO(date))
-  const findAppointmentsInSameDate = appointments.find(appointment => 
-    isEqual(parsedDate, appointment.date)
-  )
+
+  const findAppointmentsInSameDate = appointmentsRepository.findByDate(parsedDate)
 
   if(findAppointmentsInSameDate) {
     return res.status(400).json({message: "This appointment is already booked"})
   }
 
-  const appointment = new Appointment(provider, parsedDate)
 
-  appointments.push(appointment)
+  const appointment = appointmentsRepository.create(provider, parsedDate)
 
   return res.json(appointment)
 })
