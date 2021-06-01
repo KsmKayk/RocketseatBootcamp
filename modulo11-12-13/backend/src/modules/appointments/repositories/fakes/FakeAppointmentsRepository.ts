b6a136bde5b9d48b12a5ea2a364eff115a334091
Kayk <ksm.kayk@outlook.com>
2021-06-01 16:16:05 -0300

    tdd, password reset/forgot, envio de email, update de usuario

--- modulo11-12-13/backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ modulo11-12-13/backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,4 +1,4 @@
-import { v4 as uuid } from 'uuid';
+import { uuid } from 'uuidv4';
 import { isEqual } from 'date-fns';
 
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
